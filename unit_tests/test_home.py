febfb1e173633b279616de83a7249d40398b77e1
MooMan272 <mark.yasutake@gmail.com>
2017-09-08 11:33:39 -0700

    Add unit tests for FeaturedOffer object

--- unit_tests/test_home.py
+++ unit_tests/test_home.py
@@ -3,12 +3,13 @@ Unit tests for page_objects.common.
 '''
 
 import logging.config
+import time
 
 import pytest
 from selenium import webdriver
 
 import page_objects.base
-from page_objects.home import HomePageObject
+from page_objects.home import HomePageObject, FeaturedOffer
 import utils.logging_config
 logging.config.dictConfig(utils.logging_config.config)
 
@@ -45,3 +46,36 @@ class TestHomePage:
         assert len(featured_elements) > 0, "Empty list; can't verify items."
         for element in featured_elements:
             assert isinstance(element, webdriver.remote.webelement.WebElement)
+
+class TestFeaturedOffer:
+    '''
+    Tests for FeaturedOffer.
+    '''
+
+    @pytest.fixture(scope='class')
+    def featured_offer(self):
+        '''
+        Setup fixture which returns a FeaturedOffer object.
+        '''
+        home_page = HomePageObject()
+        home_page.scroll_to_bottom()
+        time.sleep(0.5)
+        featured_elements = home_page.find_featured_offer_elements()
+        # The last one is [probably] a Featured Offer.
+        return FeaturedOffer(featured_elements[-1])
+
+    def test_price(self, featured_offer):
+        '''
+        Verifies price string.
+        '''
+        price = featured_offer.price
+        logging.debug("Price: '{}'.".format(price))
+        assert price.startswith('USD $')
+
+    def test_title(self, featured_offer):
+        '''
+        Verifies title string.
+        '''
+        title = featured_offer.title
+        logging.debug("'Title: '{}'.".format(title))
+        assert title != ''
