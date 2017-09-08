b38d038231def997a186b9724f0b43aa4eef292a
MooMan272 <mark.yasutake@gmail.com>
2017-09-08 11:32:29 -0700

    Add BaseFeatured and FeaturedOffer objects

--- page_objects/home.py
+++ page_objects/home.py
@@ -2,9 +2,11 @@
 Applies to the home page.
 '''
 
+import abc
+
 from selenium.webdriver.common.by import By
 
-from page_objects.base import BasePage
+from page_objects.base import BasePage, BaseElement
 from page_objects.common import NavBar
 
 class HomePageObject(BasePage):
@@ -31,3 +33,62 @@ class HomePageObject(BasePage):
         '''
         nav_bar = NavBar()
         return nav_bar.is_loaded()
+
+class BaseFeatured(BaseElement, metaclass=abc.ABCMeta):
+    '''
+    Abstract class representing a Featured Hotel, Tour, Car, or Offer.
+    '''
+
+    def __init__(self, element):
+        '''
+        Initialize self.
+
+        Paramters
+        ---------
+        element : WebElement
+            Element which this object represents.
+        '''
+        super().__init__()
+        self.element = element
+        # These are relative to self.element
+        self.locator = {}
+        self.locator['price'] = (By.CSS_SELECTOR, 'div.featured-price')
+
+    @property
+    def price(self):
+        '''
+        Returns price.
+        '''
+        return self.element.find_element(*self.locator['price']).text
+
+    @property
+    @abc.abstractmethod
+    def title(self):
+        '''
+        Returns title.
+        '''
+        pass
+
+class FeaturedOffer(BaseFeatured):
+    '''
+    Represents a Featured Offer.
+    '''
+
+    def __init__(self, element):
+        '''
+        Initialize self.
+
+        Paramters
+        ---------
+        element : WebElement
+            Element which this object represents.
+        '''
+        super().__init__(element)
+        self.locator['title'] = (By.CSS_SELECTOR, 'div.featured-title')
+
+    @property
+    def title(self):
+        '''
+        Returns title.
+        '''
+        return self.element.find_element(*self.locator['title']).text
